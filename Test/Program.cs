057577125c6a004e9e910424ed171d23d71eb8ff
Sonali Singh <sonali.singh.ext@sivantos.com>
2020-03-26 16:32:14 +0530

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+
+namespace Test
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            TimeSpan span = new TimeSpan(0, 0, 30, 30, 0);
+            httpClient.Timeout = span;
+            string url = "http://" + "protractortestcasesnew.eastus.azurecontainer.io" + "/api/test";
+            var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseContentRead, new CancellationTokenSource(TimeSpan.FromMinutes(30)).Token);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
